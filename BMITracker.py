c4ed0b9ce0beb6c5fad3b751acefdc99234b96b6
Arman Tiwari <tiwariar279@gmail.com>
2026-02-05 22:10:33 +0530

    Add BMITracker class for BMI calculation
    Created a BMITracker class to calculate and track BMI.

---
+++ BMITracker.py
@@ -0,0 +1,5 @@
+class BMITracker:
+    # BMITracker is a python class created to calculate and track Body Mass Index (BMI)
+    def __init__(self, weight_kg, height_cm):
+        self.weight_kg = weight_kg
+        self.height_cm = height_cm
