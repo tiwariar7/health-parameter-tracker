522899cc0bf3a14a2b46aca41089ce06d592b8f2
Arman Tiwari <tiwariar279@gmail.com>
2026-02-05 22:17:39 +0530

    Update BMITracker.py

--- BMITracker.py
+++ BMITracker.py
@@ -3,3 +3,14 @@ class BMITracker:
     def __init__(self, weight_kg, height_cm):
         self.weight_kg = weight_kg
         self.height_cm = height_cm
+
+    def calculate_bmi(self):
+        # This is a python function curated especially for BMI calculation
+        # Standard Formula for BMI : BMI = weight (kg) / (height (m))^2
+        if self.height_cm <= 0:
+            raise ValueError("Height must be greater than zero.")
+        if self.weight_kg <= 0:
+            raise ValueError("Weight must be greater than zero.")
+        height_m = self.height_cm / 100
+        bmi = self.weight_kg / (height_m ** 2)
+        return bmi
