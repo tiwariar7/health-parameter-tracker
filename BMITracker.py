8e1fc45cc32e81a1a5a10fa8caf6025449654910
Arman Tiwari <tiwariar279@gmail.com>
2026-02-05 22:23:12 +0530

    Update BMITracker.py

--- BMITracker.py
+++ BMITracker.py
@@ -14,3 +14,15 @@ class BMITracker:
         height_m = self.height_cm / 100
         bmi = self.weight_kg / (height_m ** 2)
         return bmi
+
+    def bmi_category(self):
+        # This function curated tocategorize the BMI value into standard categories
+        bmi = self.calculate_bmi()
+        if bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= bmi < 24.9:
+            return "Normal weight"
+        elif 25 <= bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obesity"
