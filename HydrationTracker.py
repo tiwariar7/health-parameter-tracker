4486fbe3e7c0e19e98bb70f842e079d8b7876bb7
Arman Tiwari <tiwariar279@gmail.com>
2026-02-06 09:58:18 +0530

    Update HydrationTracker.py

--- HydrationTracker.py
+++ HydrationTracker.py
@@ -1,10 +1,19 @@
 class HydrationTracker:
   # HydrationTrackeris a python class created to calculate and track daily water intake for users
-  def __init__(self, user_name: str, weight_kg: float):
+  def __init__(self, user_name, weight_kg):
         self.user_name = user_name
         self.weight_kg = weight_kg
         self.RECOMMENDED_INTAKE_ML = calculate_recommended_intake(weight_kg)
         self.total_intake_ml = 0
-        self.intake_log = []
-  
-  
+        self.intake_log = []  
+
+    def add_water(self, amount_ml):
+        # Add water intake in milliliters
+        if amount_ml <= 0:
+            raise ValueError("Water intake must be greater than 0 ml")
+
+        self.total_intake_ml += amount_ml
+        self.intake_log.append(amount_ml)
+      
+    def get_total_intake(self):
+        return self.total_intake_ml      
